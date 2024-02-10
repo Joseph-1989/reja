d6ab7a8a35fc0d006d19dcb973672ac96410e1ff
Joseph-1989 <akmal.khasanov1989@gmail.com>
2024-02-10 10:36:25 +0900

    feat: A-TASK

--- train.js
+++ train.js
@@ -2,15 +2,15 @@
 // A-task
 // va hakozo
 // console.log("ishga tushdi");
-console.log("Jack Ma maslahatlari");
-const list = [
-  "yahshi talaba boling", //0-20
-  "togri boshliq tanlang va koproq hato qiling", // 20-30
-  "uzingizga ishlashingizni boshlang", // 30-40
-  "siz kuchli bolgan narsalarni qiling", // 40-50
-  "yoshlarga investitsiya qiling", // 50-60
-  "endi dam oling, foydasi yoq endi", // 60
-];
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//   "yahshi talaba boling", //0-20
+//   "togri boshliq tanlang va koproq hato qiling", // 20-30
+//   "uzingizga ishlashingizni boshlang", // 30-40
+//   "siz kuchli bolgan narsalarni qiling", // 40-50
+//   "yoshlarga investitsiya qiling", // 50-60
+//   "endi dam oling, foydasi yoq endi", // 60
+// ];
 
 // CALLBACK Function
 
@@ -36,29 +36,29 @@ console.log("------------CALLBACK FUNCTION------------------");
 
 console.log("------------CALLBACK FUNCTION------------------");
 
-function maslahatBering(a, callback) {
-  if (typeof a !== "number") callback("insert a number", null);
-  else if (a <= 20) callback(null, list[0]);
-  else if (a > 20 && a <= 30) callback(null, list[1]);
-  else if (a > 30 && a <= 40) callback(null, list[2]);
-  else if (a > 40 && a <= 50) callback(null, list[3]);
-  else if (a > 50 && a <= 60) callback(null, list[4]);
-  else {
-    setInterval(function () {
-      callback(null, list[5]);
-    }, 1000);
-  }
-}
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") callback("insert a number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a > 20 && a <= 30) callback(null, list[1]);
+//   else if (a > 30 && a <= 40) callback(null, list[2]);
+//   else if (a > 40 && a <= 50) callback(null, list[3]);
+//   else if (a > 50 && a <= 60) callback(null, list[4]);
+//   else {
+//     setInterval(function () {
+//       callback(null, list[5]);
+//     }, 1000);
+//   }
+// }
 
-console.log("passed here 0");
+// console.log("passed here 0");
 
-maslahatBering(70, (err, data) => {
-  if (err) console.log("ERROR:", err);
-  else {
-    console.log("javob:", data);
-  }
-});
-console.log("passed here 1");
+// maslahatBering(70, (err, data) => {
+//   if (err) console.log("ERROR:", err);
+//   else {
+//     console.log("javob:", data);
+//   }
+// });
+// console.log("passed here 1");
 
 // ASYNC Function
 
@@ -161,3 +161,21 @@ console.log("------------SETINTERVAL------------------");
 // run();
 
 console.log("------------------------------");
+
+function countLetter(letter, word) {
+  // Boshlang'ich soni
+  let count = 0;
+
+  // Har bir harfni tekshirish va sanash
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === letter) {
+      count++;
+    }
+  }
+
+  // Natijani qaytarish
+  return count;
+}
+
+// Misol: countLetter("e", "engineer") 3 ni qaytaradi
+console.log(countLetter("a", "akmal"));
